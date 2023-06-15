c6c40a342b093fee7107f0dee7d103a12800db8d
AlexKharin <A.Harin_32v@ngknn.ru>
2023-06-15 13:39:50 +0300

    v1

--- app/components/MyHeader.tsx
+++ app/components/MyHeader.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const MyHeader = () => {
   return (
     <header>
          <div>
-            <h1>Palldanerar's Blog</h1>
+            <Link href='/'>
+              <h1>Palldanerar's Blog</h1>
+            </Link>
             <p>Traveler, welcome to my personal blog!</p>
         </div>
     </header>
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import fs from "fs"
+import Markdown from 'markdown-to-jsx';
 
-const PostItem = () => {
+const getPostContent = (slug: string) => {
+    const folder = "posts/";
+    const file = `${folder}${slug}.md`;
+    const content = fs.readFileSync(file, "utf8");
+    return content;
+  };
+
+const PostItem = (props: any) => {
+  const slug = props.params.slug
+  const content = getPostContent(slug)
+
   return (
-    <div>PostItem</div>
+    <div>
+        <h2>{slug}</h2>
+        <Markdown>
+            {content}
+        </Markdown>
+    </div>
   )
 }
 
